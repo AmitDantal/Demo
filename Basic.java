34272fcd6a5a73247b0f33904376044413d7dfff
AmitDantal <143423292+AmitDantal@users.noreply.github.com>
2023-11-28 11:51:47 +0530

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,13 @@
+package com.java.basic;
+
+public class Basic 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World.");
+		System.out.println("My name is Amit Anil Dantal.");
+		System.out.println("I am from Kale,Kolhapur.");
+		System.out.println("Currantly I am in final Year B.Tech in CSE.");
+		System.out.println("My Hobbies are Playing online games.");
+	}
+}
